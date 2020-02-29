aa1cffabb0daedb75af4dde5853cff842f187347
sensfre <sensfre@k07.itscom.net>
2020-02-29 15:25:55 +0900

    b_trend を trend_util 利用に変更

--- script/b_trend.py
+++ script/b_trend.py
@@ -12,6 +12,7 @@ import matplotlib.dates as mdates
 
 from fileio import dt_fm_sn, sn_fm_dt
 from polldb import DB, interp, t_min, t_max, calc_fact
+from trend_util import TVEN, trend_mav, trend_sunday, deviation, set_date_tick
 from db_defs import db_defs
 
 cfg = {
@@ -19,36 +20,11 @@ cfg = {
     'args': None,    # parse_args() の戻り値を保持する
 }
 
-def deviation(tt, yy, yi):
-    yc = np.array([yi(t) for t in tt])
-    yd = yy - yc
-    sd = np.std(yd, ddof=1)
-    return yd, sd
-    
-class TVEN:
-    """ time, value, error, num
-    
-    Bugs
-    ----
-    num > 0 で有効としている。
-    error に標準偏差を採用している場合、num == 1 の時に nan 以外を要求する。
-    
-    """
-    def __init__(self, tim, val, err, num):
-        self.raw = zip(tim, val, err, num)
-        self.buf = self.select_valid(self.raw)
-
-    def select_valid(self, raw):
-        return [a for a in raw if a[3] > 0] # 
-        
-    def by_column(self):
-        return [np.array(a) for a in zip(*self.buf)]
-        
-        
 tven_buf = {}
 
 def proc_raw_cal_sdv(fc_dict, axes, column, k_app_nap, pp):
-    
+    """ 発表値、補正値、補正値の残差のグラフ
+    """
     tim, val, err, num = tven_buf[k_app_nap].by_column()
     
     ax =axes[0, column]
@@ -90,7 +66,9 @@ def proc_raw_cal_sdv(fc_dict, axes, column, k_app_nap, pp):
     ax.grid(True)
     ax.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
     
-def proc_summary():
+def proc_trend():
+    """ サマリー  (↑支持, ↓不支持 グラフ)
+    """
     args = cfg['args']
     
     fig, ax1 = plt.subplots(figsize=(8, 5))
@@ -159,7 +137,9 @@ def proc_summary():
         fig.savefig(os.path.join(args.gout_folder, 'Fig%d_%s.png' % (args.gout_ndx + 0, cfg['gout_date'])))
     
     
-def proc_summary_x(db_list, fc_dict):
+def proc_trend_x(db_list, fc_dict):
+    """ サマリーX  (↑支持, ↑不支持 グラフ)
+    """
     args = cfg['args']
     
     fig, ax = plt.subplots(figsize=(8, 5))
@@ -218,130 +198,16 @@ def proc_summary_x(db_list, fc_dict):
     if args.gout:
         fig.savefig(os.path.join(args.gout_folder, 'Fig%d_%s.png' % (args.gout_ndx + 1, cfg['gout_date'])))
     
-def set_date_tick(ax, bymonth, format, rotate):
-    """ 日付軸の設定
+def proc_factor(db_list, k_app_nap, k_title, fc_dict, gn_rel):
+    """ 報道10 社の支持率/不支持率の調査感度の時系列比較
     
-    ax : axis
-    bymonth : 表示する月のリスト。 ex : [1, 4, 7, 10]
-    format : 表示形式。 ex : '%Y/%m'
-    rotate : [deg] 文字の回転角。
-    
-    """
-    ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=bymonth))
-    ax.xaxis.set_minor_locator(mdates.MonthLocator())
-    ax.xaxis.set_major_formatter(mdates.DateFormatter(format))
-    
-    # 'ax.xaxis.set_tick_params(rotation=45)' does not work ?
-    if rotate != 0:
-        for label in ax.get_xmajorticklabels():
-            label.set_rotation(rotate)
-            label.set_horizontalalignment("right")
-    
-def calc_mav(fc_dict, yn, t_node, db_list, w_days, k_days):
-    """
-    w_days : [day]  +/-w_days のデータを使う
-    k_days : [day] 指数移動平均の時定数
-    """
-    _tt = []
-    _vv = []
-    for db in db_list:
-        _tt = _tt + list(db.db['T'])
-        ff = [fc_dict[yn][db.label](t) for t in db.db['T']]
-        vv = [a/b for a, b in zip(db.db[yn], ff)]
-        _vv = _vv + list(vv)
-    _tt = np.array(_tt)
-    _vv = np.array(_vv)
-    tt_min = min(_tt)
-    
-    def _mav(t):
-        # 時刻 t における窓付き指数移動平均
-        #     w_days : [day] 窓幅
-        #     k_days : [day] 時定数
-        #
-        w_post = max(0, tt_min + w_days - t)
-        if 1:
-            w_post = w_days
-        ndx = (_tt >= t - w_days) & (_tt <= t + w_post)
-        tt = _tt[ndx]
-        vv = _vv[ndx]
-        num = len(tt)
-        if num >= 2:
-            ww = np.exp(-np.abs(tt - t)/k_days)
-            avg = np.sum(ww*vv)/np.sum(ww)
-        else:
-            avg = np.NaN
-        return avg, num
-        
-    # 全社平均(指数移動平均)を求める。
-    #  f_mav(t) は補間関数
-    # t_node = [a for a in sorted(np.arange(t_max(db_list), t_min(db_list), -2))]
-    v_node, n_node = zip(*[_mav(a) for a in t_node])
-    v_node = np.array(v_node)
-    
-    f_mav = interp(t_node, v_node)
-    
-    def _err(t):
-        w_post = max(0, tt_min + w_days - t)
-        if 1:
-            w_post = w_days
-        ndx = (_tt >= t - w_days) & (_tt <= t + w_post)
-        tt = _tt[ndx]
-        vv = _vv[ndx]
-        d = [v - f_mav(t) for t, v in zip(tt, vv)]
-        return np.std(d, ddof=1)
-    e_node = [_err(a) for a in t_node]
-    
-    return TVEN(t_node, v_node, e_node, n_node)
-    
-    
-def calc_every_sunday(fc_dict, yn, t_node, db_list):
-    """
-    """
-    _tt = []
-    _vv = []
-    for db in db_list:
-        _tt = _tt + list(db.db['T'])
-        ff = [fc_dict[yn][db.label](t) for t in db.db['T']]
-        vv = [a/b for a, b in zip(db.db[yn], ff)]
-        _vv = _vv + list(vv)
-        
-    tv = [(a,b) for (a,b) in zip(_tt, _vv) if not np.isnan(b)]
-    _tt, _vv = zip(*tv)
-    _tt = np.array(_tt)
-    _vv = np.array(_vv)
-    
-    def _avg(t):
-        # t +/-2 day の観測値の平均を求める
-        ndx = (_tt >= t - 3) & (_tt <= t + 3)
-        tt = _tt[ndx]
-        vv = _vv[ndx]
-        n = len(vv)
-        if n == 0:
-            v = np.NaN
-            e = np.NaN
-        elif n == 1:
-            v = np.mean(vv)
-            e = 3.5/2 # [pt]  1σ 相当
-        else:
-            v = np.mean(vv)
-            e = max(np.std(vv, ddof=1), 3.5/2)/np.sqrt(n)
-        return v, e, n
-        
-    v_node, e_node, n_node = zip(*[_avg(t) for t in t_node])
-    v_node = np.array(v_node)
-    e_node = np.array(e_node)
-    n_node = np.array(n_node)
-    
-    return TVEN(t_node, v_node, e_node, n_node)
-    
-def proc_factor_mav(db_list, k_app_nap, k_title, fc_dict, gn_rel):
-    """
     Parameters
     ----------
     db_list, list of DB()
     k_app_nap, string : 分析データキー ('APP_RATE' or 'NAP_RATE')
     k_title, string : グラフタイトル
-    
+    fc_dict, - : 他の分析で使っている感度係数補間関数の辞書
+    gn_rel, : 出力図番の相対
     """
     args = cfg['args']
     
@@ -355,7 +221,7 @@ def proc_factor_mav(db_list, k_app_nap, k_title, fc_dict, gn_rel):
     t0 = sn_fm_dt(_d(cfg['args'].db_begin))
     tf = t_max(db_list)
     
-    # 窓幅をいくつか選んで感度のトレンドを描画(調査会社毎)
+    # 移動平均の窓幅をいくつか選んで感度のトレンドを描画(調査会社毎)
     for ndx, m in enumerate([3, 6]):  # window size (m-month)
         tt = np.arange(t0 + 6*30, tf, 7)
         ff = calc_fact(db_list, k_app_nap, tt, m*30)
@@ -461,16 +327,16 @@ def main():
         if args.k_days > 0:
             t0 = sn_fm_dt(d0)
             t_node = np.arange(t0, t_max(ppa) + 1, 1) # 移動平均を求める時刻
-            tven_buf[k] = calc_mav(fc_dict, k, t_node, ppa, w_days=30, k_days=args.k_days)
+            tven_buf[k] = trend_mav(fc_dict, k, t_node, ppa, w_days=30, k_days=args.k_days)
         else:
             t0 = sn_fm_dt(d0)
             t0_sunday = t0 + (6 - d0.weekday())  # 0:月曜  6:日曜
             t_node = np.arange(t0_sunday, t_max(ppa) + 1, 7) # 移動平均を求める時刻
-            tven_buf[k] = calc_every_sunday(fc_dict, k, t_node, ppa)
+            tven_buf[k] = trend_sunday(fc_dict, k, t_node, ppa)
         
     if 1:
-        proc_summary()
-        proc_summary_x(ppa, fc_dict)
+        proc_trend()
+        proc_trend_x(ppa, fc_dict)
     
     if 1:
         # 公表値/補正値/残差
@@ -497,8 +363,8 @@ def main():
              fig.savefig(os.path.join(args.gout_folder, 'Fig%d_%s.png' % (args.gout_ndx + 2, cfg['gout_date'])))
         
     if 1:
-        proc_factor_mav(ppa, 'APP_RATE', '内閣 支持率 感度係数', fc_dict, 3)
-        proc_factor_mav(ppa, 'NAP_RATE', '内閣 不支持率 感度係数', fc_dict, 4)
+        proc_factor(ppa, 'APP_RATE', '内閣 支持率 感度係数', fc_dict, 3)
+        proc_factor(ppa, 'NAP_RATE', '内閣 不支持率 感度係数', fc_dict, 4)
         
     # 表示
     #    イメージファイルの出力は proc_last() 内で行う
